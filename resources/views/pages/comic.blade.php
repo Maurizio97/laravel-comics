4c673992ffb54a5edeb7d4b745e3386f57ca5c9c
Maurizio <proiettomaurizio97@gmail.com>
2022-01-21 14:59:49 +0100

    comic page and routes complete

--- resources/views/pages/comic.blade.php
+++ resources/views/pages/comic.blade.php
@@ -1,9 +1,10 @@
 @extends('layout.main-layout')
+
 @section('component')
-    <h1 id="title">
-        Home Page
+    <h1>
+        Page comic
     </h1>
     <div class="anchor">
-        <a href="{{ route('comic')}}">COMIC</a>
+        <a href="{{ route('home') }}">HOME</a>
     </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('pages.home');
-});
+}) -> name('home');
+
+Route::get('/comic', function () {
+    return view('pages.comic');
+}) -> name('comic');
+
+
