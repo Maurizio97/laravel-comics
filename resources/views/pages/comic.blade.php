797c2b1e73835b3ce969560dad397f5215262ea2
Maurizio <proiettomaurizio97@gmail.com>
2022-01-24 16:25:15 +0100

    banner layout

--- resources/views/pages/comic.blade.php
+++ resources/views/pages/comic.blade.php
@@ -3,7 +3,16 @@
 @section('component')
 <!-- plate -->
 <div id="banner">
-    <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="cover">
+    <div id="cont-img-banner">
+
+        <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="cover">
+        <div id="comic-book">
+            COMIC BOOK
+        </div>
+        <div id="view-gallery">
+            VIEW GALLERY
+        </div>
+    </div>
 </div>
 <!-- /plate -->
 <section id="comic-page">
